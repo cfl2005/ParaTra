e115c8dd68d06a71112540a5669b661ea07f4797
Felix CAI <104542475@qq.com>
2022-04-27 17:45:56 +0800

    Update predict_process_n.py

--- test/predict_process_n.py
+++ test/predict_process_n.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #coding:utf-8
 
-__author__ = 'xmxoxo<xmxoxo@qq.com>'
-
 import argparse
 import os
 import re
@@ -29,11 +27,10 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import warnings
 warnings.filterwarnings('ignore')
 
-nvmlInit() #初始化
+nvmlInit() 
 
 def translate_batch_sub(sentences, model=None, batch_size=64, show_result=0, tmr_queue=None):
     start = time.time()
-    # 加载模型
     if model is None:
         model = make_model(config.src_vocab_size, config.tgt_vocab_size, config.n_layers,
                            config.d_model, config.d_ff, config.n_heads, config.dropout)
@@ -79,19 +76,16 @@ if __name__ == '__main__':
         print('data file error')
         sys.exit()
     
-    # 启动显存监测记录
     obj = GPU_MEM()
     obj.build()
     obj.start()
 
-    # 开始计时
     start = time.time()
     print(' NMT Task: Multi Process Batch '.center(40,'-'))
     print('total processes:%d' % num_process)
     print('total sentences:%d' % total_sent)
     print('Building model...')
 
-    # 加载模型
     model = make_model(config.src_vocab_size, config.tgt_vocab_size, config.n_layers,
                        config.d_model, config.d_ff, config.n_heads, config.dropout)
     model.share_memory()
@@ -101,14 +95,11 @@ if __name__ == '__main__':
     loadstime = (time.time() - start)*1000
     time_queue = mp.Queue()
 
-    # 开始计时
     start = time.time()
     print('Create process...')
     process_list = []
     for i in range(num_process):
-        # 主进程加载模型及权重，共享给子进程
         p = mp.Process(target=translate_batch_sub, args=(sentences, model, batch_size, show_result, time_queue))
-        # 在子进程中加载模型定义及权重
         # p = mp.Process(target=translate_batch_sub, args=(sentences, None, batch_size, show_result, time_queue ))
         process_list.append(p)
 
@@ -121,7 +112,6 @@ if __name__ == '__main__':
     for i in range(num_process):
         process_list[i].join()
 
-    # 停止显存监测
     obj.stop()
     # print('mem data:', obj.data)
     print('mem_ave:', obj.mem_ave())
@@ -136,14 +126,10 @@ if __name__ == '__main__':
         process_times.append(tmr)
     ave_process_time = np.average (process_times)
     
-    print('加载模型用时:%.3f 毫秒' % loadstime )
+    print('load model time:%.3f 毫秒' % loadstime )
     print('Used Memory:%d MB' % memory)
-    print('预测总计用时:%.3f 毫秒' % predict_time )
-    print('预测单句用时:%.3f 毫秒' % avetime )
-    print('进程用时(毫秒):%s ' % process_times)
-    print('进程平均用时:%.3f 毫秒' % ave_process_time)
-    print('平均单句用时:%.3f 毫秒' % (ave_process_time / total_sent))
-    print('任务总计用时:%.3f 毫秒' % (predict_time + loadstime) )
+    print('predict time:%.3f 毫秒' % predict_time )
+    print('predict single sentence time:%.3f 毫秒' % avetime )
 
     result = {'name':'MultiProcess', 'num_process':num_process, 'total_sent':total_sent,
                 'memory':memory, 'loadstime': loadstime, 'predict_time':predict_time,
@@ -151,7 +137,5 @@ if __name__ == '__main__':
                 'process_times':process_times, 'ave_process_time':ave_process_time,                
                 'mem data':obj.data, 'mem_ave': obj.mem_ave(), 'mem_max':obj.mem_max()
              }
-
-    # 追加到日志文件
     savetofile(json.dumps(result), logfile)
 
